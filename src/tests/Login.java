1533d3f286a1614a35ba98f1089e0cd5dd70bcc6
Alex-Starik <oleksandrstarik@gmail.com>
2020-04-23 15:54:21 -0400

    add Login.java

---
+++ src/tests/Login.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("believe me, this fall I will be The Best SDET in The Best, Biggest Company of the USA!!!");
+    }
+}
