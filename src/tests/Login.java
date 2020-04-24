9a955d5f0e3f79e9de3646c02d5dc7ec48e3a20f
Alex-Starik <oleksandrstarik@gmail.com>
2020-04-24 14:31:55 -0400

    Test.js

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -3,5 +3,6 @@ package tests;
 public class Login {
     public static void main(String[] args) {
         System.out.println("believe me, this fall I will be The Best SDET in The Best, Biggest Company of the USA!!!");
+        // Yes
     }
 }
